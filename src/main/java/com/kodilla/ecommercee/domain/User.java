dce72a0f8eed929c7417390d9bc5aef14b01c0b8
Piotr-Trzcinski <cinekenic@go2.pl>
2025-05-13 14:19:00 +0200

    another fix

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,30 +1,19 @@
 package com.kodilla.ecommercee.domain;
 
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "email", nullable = false)
     private String email;
-
-    @Column(name = "is_blocked")
     private boolean isBlocked;
-
-    @Column(name = "created_at")
     private LocalDateTime createdAt;
 }
