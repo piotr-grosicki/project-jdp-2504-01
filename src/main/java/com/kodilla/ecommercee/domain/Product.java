0acf06bda5d76c0cfc1a03033efa860cae4a7adf
Piotr-Trzcinski <cinekenic@go2.pl>
2025-05-15 15:39:54 +0200

    Product

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,12 +1,15 @@
 package com.kodilla.ecommercee.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.*;
 import lombok.Getter;
-import org.springframework.data.annotation.Id;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
+@Entity
+@Table(name = "products")
 @Getter
+@Setter
+@NoArgsConstructor
 public class Product {
 
     public Product(String name, String description, Double price, ProductAvailability productAvailability) {
@@ -14,15 +17,27 @@ public class Product {
         this.description = description;
         this.price = price;
         this.productAvailability = productAvailability;
+//        this.productGroup = productGroup;
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name", nullable = false)
     private String name;
 
+    @Column(name = "description")
     private String description;
 
+    @Column(name = "price", nullable = false)
     private Double price;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "availability", nullable = false)
     private ProductAvailability productAvailability;
+
+//    @ManyToOne
+//    @JoinColumn(name = "product_group_id", nullable = false)
+//    private ProductGroup productGroup;
 }
