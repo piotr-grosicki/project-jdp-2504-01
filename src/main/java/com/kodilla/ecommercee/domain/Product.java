52f861073381c189e6e5453573ad02fd124398ac
Piotr-Trzcinski <cinekenic@go2.pl>
2025-05-19 18:35:18 +0200

    ProductRepository

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,6 +1,7 @@
 package com.kodilla.ecommercee.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,15 +11,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Product {
-
-    public Product(String name, String description, Double price, ProductAvailability productAvailability) {
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.productAvailability = productAvailability;
-//        this.productGroup = productGroup;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,14 +35,5 @@ public class Product {
 //    @JoinColumn(name = "product_group_id", nullable = false)
 //    private ProductGroup productGroup;
 
-    //metody tymczasowe pomocnicze
-    public static Product fromDto(ProductDto dto){
-        Product product = new Product(dto.getName(), dto.getDescription(), dto.getPrice(), ProductAvailability.AVAILABLE);
-        product.setId(dto.getId());
-        return product;
-    }
 
-    public ProductDto toDto() {
-        return new ProductDto(id, name, description, price);
-    }
 }
