4a24323699ed16c7196a78f76e5021f3250b9f72
NeoBTK <neobtk@gmail.com>
2025-05-22 00:53:52 +0200

    JDP250401-15: Fixed. Some changes added to CartController and building project works ;)

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -42,6 +42,8 @@ public class CartController {
 
     @PostMapping("/{cartId}")
     public Order createOrder(@PathVariable Long cartId) {
-        return new Order(1L, BigDecimal.valueOf(100.00), "Elm street", LocalDateTime.now(), OrderStatus.COMPLETED);
+        return new Order(1L, BigDecimal.valueOf(100.00), "Elm street", LocalDateTime.now(), OrderStatus.COMPLETED,
+                new User(1L, "FirstName", "LastName", "email@email.com", false, LocalDateTime.now(), new ArrayList<>(),
+                        new Cart(1L, 1L, List.of())));
     }
 }
